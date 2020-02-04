a8ccaef0cd97937fb7627daa8f80e189d9c14a03
github username <github email address>
2020-02-03 17:50:34 -0800

    Done.

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,10 +1,47 @@
 class Artist
-
   attr_reader :name, :years_experience
+  @@all = []
 
   def initialize(name, years_experience)
     @name = name
     @years_experience = years_experience
+    @@all << self
   end
 
+  def self.all
+    @@all
+  end
+
+  def paintings
+    Painting.all.select { |painting| painting.artist == self }
+  end
+
+  def galleries
+    self.paintings.map do |painting|
+      painting.gallery
+    end.uniq
+  end
+
+  def cities
+    self.galleries.map do |gallery|
+      gallery.city
+    end.uniq
+  end
+
+  def self.total_experience
+    x = all.map { |artist| artist.years_experience }
+    return x.sum
+  end
+
+  def self.most_prolific
+    tally = {}
+    self.all.each { |artist|
+      tally[artist] = (artist.paintings.length.to_f) / (artist.years_experience.to_f)
+    }
+    tally.each { |k, v| return k if v == tally.values.max }
+  end
+
+  def create_painting(title, price, gallery)
+    Painting.new(title, price, self, gallery)
+  end
 end
--- tools/console.rb
+++ tools/console.rb
@@ -1,7 +1,18 @@
-require_relative '../config/environment.rb'
+require_relative "../config/environment.rb"
 
+picasso = Artist.new("Picasso", 17)
+van_gogh = Artist.new("Van Gogh", 34)
+okeefe = Artist.new("O'Keefe", 22)
+agnes = Artist.new("Agnes Martin", 25)
 
+met = Gallery.new("Metropolian", "New York")
+moma = Gallery.new("MoMA", "New York")
+natl = Gallery.new("National Gallery of Art", "DC")
 
+blue = Painting.new("blue", 1000, picasso, met)
+red = Painting.new("red", 5000, picasso, moma)
+yellow = Painting.new("yellow", 4000, agnes, moma)
+green = Painting.new("green", 3209, van_gogh, natl)
 
 binding.pry
 
